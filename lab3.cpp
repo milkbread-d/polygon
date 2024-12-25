f47c7709936a66f9532338f4a29f505564ef8f3e
milkbread-d <ayan.dzh178@gmail.com>
2024-12-25 23:12:23 +0300

    lab-3

--- lab3.cpp
+++ lab3.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
 
 
 bool coro(const string& str){
-  int
+  int state = 0;
+  for(int i = 0; i < str.length(); i++){
+    char current = str[i];
+    if (current == '('){
+      state++;
+    }
+    else if (current == ')'){
+    state--;
+    }
+
+  }
+  return state != 0;
+}
+
+
+int main(){
+  string str;
+  ifstream fin("lab3.txt");
+  fin>>str;
+
+  ofstream fout("lab3_out.txt");
+  fout << coro(str);
+  cout<< coro(str);
+  fout.close();
+  fin.close();
+}
